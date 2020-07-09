8b9b6c6f0f31384d9c5d125f8e0f77ef7de81ac2
zhangkanglei <wb-zkl690803@alibaba-inc.com>
2020-07-09 17:56:40 +0800

    最后一个单词的长度

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,27 @@
 import java.util.*;
 
 public class Solution {
-    public static int[] runningSum(int[] nums) {
-        int total = 0;
-        for (int i = 0; i < nums.length; i++) {
-            total += nums[i];
-            nums[i] = total;
+    public static int lengthOfLastWord(String s) {
+
+        if (s == null || s.length() == 0) {
+            return 0;
         }
-        return nums;
+
+        s = s.trim();
+
+        int i = s.lastIndexOf(" ");
+
+        if (i == -1) {
+            return s.length();
+        }
+
+        String lastWord = s.substring(i, s.length() - 1);
+
+        return lastWord.length();
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(Solution.runningSum(new int[]{3,1,2,10,1})));
+        System.out.println(lengthOfLastWord("a "));
     }
 
 
